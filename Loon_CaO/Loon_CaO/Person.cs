8216d4f549736eb7094a74c911a03050049fd8bc
ACT-STUDENT <ACT-STUDENT@LAB3-PC27>
2025-08-29 14:46:44 +0800

    Exercise 1

---
+++ Loon_CaO/Loon_CaO/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_CaO
+{
+    internal class Person
+    {
+        //Class member
+        public int age;
+        public string name;
+        public string gender;
+
+        public void Greet()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" Hello good afternoon everyone and good afternoon Ms Farah, My name is " + name + " I'm " + age + "years old, and my gender is " + gender);
+        }
+    }
+}
---
+++ Loon_CaO/Loon_CaO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_CaO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sky Loon
+            //IT306
+            //Exercise 1: Creating a class 
+
+            //Object instantiation
+            Person person1 = new Person();
+
+            //initialization of fields 
+            person1.age = 18;
+            person1.gender = "MALE";
+            person1.name = "Sky Loon";
+
+            //call the method
+            person1.Greet();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
