617e0b60f653cdf93f79b5f28eff8f7415917d8c
ACT-STUDENT <ACT-STUDENT@LAB3-PC27>
2025-08-29 14:15:43 +0800

    Constructor UserInput

---
+++ Loon_COContructorUserInput/Loon_COContructorUserInput/Foods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_COContructorUserInput
+{
+    internal class Foods
+    {
+        //Fields
+        public string pig, fish, chicken;
+
+
+        //Parameterized constructor
+        public Foods(string pigFood, string fishFood, string chickenFood)
+        {
+            pig = pigFood;
+            fish = fishFood;
+            chicken = chickenFood;
+        }
+
+        // Methods to display food details
+        public void DisplayFoodDetail()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Here are the favorite food you want in this different meat of animals");
+            Console.WriteLine("\nYour favorite Pig Food: " + pig);
+            Console.WriteLine("Your favorite Fish Food: " + fish);
+            Console.WriteLine("Your favorite Chicken Food: " + chicken);
+        }
+    }
+}
---
+++ Loon_COContructorUserInput/Loon_COContructorUserInput/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_COContructorUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //Sky Loon
+        //IT306
+        //Exercise 3:User Input with Constructor
+
+        Main:
+            //Ask user to input favorite foods
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\tThis program will ask if what is your Favorite Food");
+            Console.Write("\nWhat is your favorite Fish Food: ");
+            string fishFood = Console.ReadLine();
+            Console.Write("\nWhat is your favorite Pig Food: ");
+            string pigFood = Console.ReadLine();
+            Console.Write("\nWhat is your favorite Chicken Food: ");
+            string chickenFood = Console.ReadLine();
+
+            //instantiation of a class object
+            Foods foods = new Foods(fishFood, pigFood, chickenFood);
+
+            //call the method to display detail
+            foods.DisplayFoodDetail();
+
+            //ask the user to input another
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\nDo you want to input another favorite food? (y/n) ");
+            char choice = Convert.ToChar(Console.ReadLine());
+            if (choice == 'y')
+            {
+                goto Main;
+            }
+            else if (choice == 'n')
+            {
+                return;
+            }
+
+        }
+    }
+}
