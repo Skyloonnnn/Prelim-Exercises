caa0bc0cec3b0ebb2cce9bb0d1fe62f797edc4ec
ACT-STUDENT <ACT-STUDENT@LAB3-PC27>
2025-08-29 14:30:45 +0800

    Exercise 2

---
+++ Loon_MyClassAndObject/Loon_MyClassAndObject/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_MyClassAndObject
+{
+    internal class Food
+    {
+        //attributes
+        public string pig, fish, chicken;
+
+        //method to display
+        public void DisplayFood()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\tFilipino Food Recipes ");
+
+            Console.ForegroundColor= ConsoleColor.Cyan;
+            Console.WriteLine("\nPig Food: " + pig + "\nFish Food: " + fish + "\nChicken: " + chicken); 
+        }
+    }
+}
---
+++ Loon_MyClassAndObject/Loon_MyClassAndObject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_MyClassAndObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //object instantiation
+            Food food1 = new Food();
+            Food food2 = new Food();
+
+            //initialize fields
+            food1.pig = "Letchon Baboy";
+            food1.fish = "Pritong Isda";
+            food1.chicken = "Letchon Manok";
+
+            food2.pig = "Pork Adobo";
+            food2.fish = "Sinigang na Bangus";
+            food2.chicken = "Chicken Adobo";
+
+            //call the method to display
+            food1.DisplayFood();
+            food2.DisplayFood();
+
+            Console.ReadKey();
+        }
+    }
+}
