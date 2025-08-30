b02324eeca5095c37b3b7555191bf84fca476cf4
My Laptop <My Laptop@DESKTOP-0U8QNKN>
2025-08-30 15:36:27 +0800

    DADA

---
+++ SkyLoon/SkyLoon/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyLoon
+{
+    internal class Person
+    {
+        public string name;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Name: " + name);
+        }
+    }
+}
---
+++ SkyLoon/SkyLoon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyLoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.name = "Sky Loon";
+
+            person.DisplayInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
